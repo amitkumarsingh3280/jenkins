c8275e67e8e3d2603ca694b2b87f5a6011f14c57
Ubuntu <ubuntu@ip-172-31-16-130.ec2.internal>
2023-06-21 18:35:39 +0000

    comitting master branch file

---
+++ main.sh
@@ -0,0 +1,2 @@
+Hello
+This is prod job
---
+++ master.sh
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Second WebPage</title>
+</head>
+<body>
+<p>Click on the "Choose File" button to upload a file:</p>
+
+<form action="/action_page.php">
+  <input type="file" id="myFile" name="filename">
+  <input type="submit">
+</form>
+
+</body>
+</html>
