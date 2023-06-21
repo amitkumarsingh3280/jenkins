21d0ff8b8cb418597c85ebe52bc29617584c708f
Ubuntu <ubuntu@ip-172-31-16-130.ec2.internal>
2023-06-21 18:51:19 +0000

    comitting both file

--- develop.sh
+++ develop.sh
@@ -1,1 +1,3 @@
 This is jenkins assignment
+
+Hello This is third assignment and job would be run auto
--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
 Hello Guys
 This is jenkins project and job is run auto
+
+This is third assignment and job would be run auto
